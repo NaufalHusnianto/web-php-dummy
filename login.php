c0ec2824a517c5d23f943d6a5d7f8680d4faa8b1
Candra Zulkarnain <candrazulkarnain8@gmail.com>
2026-01-01 20:41:33 +0700

    tambah csrf token ke halaman login

--- login.php
+++ login.php
@@ -1,6 +1,16 @@
 <?php
 require_once("config.php");
 
+if (empty($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'] ?? '')) {
+        die('CSRF token tidak valid');
+    }
+}
+
 $error = "";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
